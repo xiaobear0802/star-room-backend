c549b616cf715817078859d5e9ae7a742056cb6a
xiaobear0802 <babybear960802@example.com>
2025-08-06 14:42:56 +0800

    更新

--- socketHandlers.js
+++ socketHandlers.js
@@ -33,15 +33,13 @@
         const getIo = () => globalIo;
 
         module.exports = { initializeSocketIO, getIo };
-        function initializeSocketIO(io) {
+       function initializeSocketIO(io) {
   io.on('connection', socket => {
-    console.log('使用者已連線:', socket.id);
+    console.log('Socket connected:', socket.id);
 
     socket.on('disconnect', () => {
-      console.log('使用者已離線:', socket.id);
+      console.log('Socket disconnected:', socket.id);
     });
-
-    // 你可以在這裡加入更多事件處理邏輯
   });
 }
 
